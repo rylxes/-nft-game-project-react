f75f34fee7d15b8a8d5f2dd57969e29474cbbce5
Agboola Sherriff <rylxes@gmail.com>
2021-12-05 09:33:12 +0000

    Initial commit

---
+++ src/constants.jsx
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x220d80C98E52d28E5FDd67750754EF0aD86E9c54';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
